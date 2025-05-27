3d1c13e186593645fd883cafeb8139d772b2c1f6
Souparna_COCO <115988950+SouparnaChatterjee@users.noreply.github.com>
2025-05-27 22:56:24 +0530

    Create script.js

---
+++ 27.5.25/7/script.js
@@ -0,0 +1,11 @@
+// Fetch users from JSONPlaceholder and log their names
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => {
+    users.forEach(user => {
+      console.log(user.name);
+    });
+  })
+  .catch(error => {
+    console.error("Error fetching users:", error);
+  });
