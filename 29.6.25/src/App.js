e832659230647799d053fad674e898645a50e7ae
Souparna_COCO <115988950+SouparnaChatterjee@users.noreply.github.com>
2025-06-29 21:03:19 +0530

    Create App.js

---
+++ 29.6.25/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorToggleButton from './ColorToggleButton';
+
+function App() {
+  return (
+    <div>
+      <ColorToggleButton />
+    </div>
+  );
+}
+
+export default App;
