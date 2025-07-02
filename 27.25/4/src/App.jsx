1d17a20d4c1e9e3bc41818ea70f7f935b2218d70
Souparna_COCO <115988950+SouparnaChatterjee@users.noreply.github.com>
2025-07-02 23:55:58 +0530

    Create App.jsx

---
+++ 27.25/4/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AutoCorrectApp from './AutoCorrectApp';
+
+const App = () => {
+  return (
+    <div>
+      <h1>AutoCorrect App</h1>
+      <AutoCorrectApp />
+    </div>
+  );
+};
+
+export default App;
