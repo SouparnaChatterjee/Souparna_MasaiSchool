4079c934b6f7a067258f5f43f46ee053b3edf1c3
Souparna_COCO <115988950+SouparnaChatterjee@users.noreply.github.com>
2025-07-13 18:52:05 +0530

    Create store.js

---
+++ 13.7.25/13/src/redux/store.js
@@ -0,0 +1,9 @@
+import { legacy_createStore as createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { coffeeReducer } from "./coffee/reducer";
+
+const rootReducer = combineReducers({
+  coffee: coffeeReducer,
+});
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
