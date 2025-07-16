7b600a57dfb3927b20dbab49be9c2b5c353dca04
Souparna_COCO <115988950+SouparnaChatterjee@users.noreply.github.com>
2025-07-16 23:12:43 +0530

    Create App.js

---
+++ 16.7.25/1/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tracker from './components/Tracker';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MindTrack – Daily Journal</h1>
+      <Tracker />
+    </div>
+  );
+}
+
+export default App;
