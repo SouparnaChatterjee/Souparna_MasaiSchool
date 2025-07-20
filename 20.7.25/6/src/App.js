488436805c81d9c1735b2e6ae57d92fa621146e4
Souparna_COCO <115988950+SouparnaChatterjee@users.noreply.github.com>
2025-07-20 21:02:18 +0530

    Create App.js

---
+++ 20.7.25/6/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignupForm from './components/SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center' }}>Custom Hooks Demo</h1>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
