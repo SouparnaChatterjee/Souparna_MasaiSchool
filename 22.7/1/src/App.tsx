701183a108f5451e5f134e7b6fdbaf7a39af9543
Souparna_COCO <115988950+SouparnaChatterjee@users.noreply.github.com>
2025-07-22 23:03:50 +0530

    Create App.tsx

---
+++ 22.7/1/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FeedbackForm from './components/FeedbackForm';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <FeedbackForm />
+    </div>
+  );
+};
+
+export default App;
